a48f7bed1ebe41936bbd3974431d277ea2498f24
Matías Reyes <matreyes@gmail.com>
2019-12-15 19:24:31 -0300

    Use a json document as definition and choose between different questions for each letter.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,58 @@
+const questions = {
+	"A": [
+		["Nombre científico de helminto cuyas larvas alcanzan el tejido pulmonar pudiendo causar tos, crisis obstructivas y hemoptisis.", "ascaris lumbricoides"],
+		["Planta grande", "arbol"]
+	],
+	"B": [
+		["barco pequeño", "bote"],
+		["pelo de la cara", "braba"]
+	]
+}
+const questions_array = Object.entries(questions)
+
+const words = questions_array.map(function (v, index) {
+	var selected = v[1][Math.floor(Math.random() * v[1].length)]
+	return createWord(index, v[0], selected[0], selected[1])
+})
+
 // Variables
 // -----------------------------------------------------------------------------
 
-var words = [
-	new Word(0, "A", "Empieza por A:", " Nombre científico de helminto cuyas larvas alcanzan el tejido pulmonar pudiendo causar tos, crisis obstructivas y hemoptisis.", "ascaris lumbricoides"),
-	new Word(1, "B", "Empieza por B:", " Entidad clínica  caracterizada por fiebre sin foco, sed orina no inflamatorio, GB >15000 y PCR >90", "bacteremia oculta"),
-	new Word(2, "C", "Empieza por C:", " Agente infeccioso cuya infección in útero puede producir sordera en el niño", "citomegalovirus"),
-	new Word(3, "D", "Empieza por D:", " Antimicrobiano usado en el tratamiento del acné, rosácea, infecciones por Rickettsias, brucella canis y gonorrea", "doxiciclina"),
-	new Word(4, "E", "Empieza por E:", " Forma de presentación de la infección neurológica por virus herpes", "encefalitis"),
-	new Word(5, "F", "Empieza por F:", " Infección cutánea por S. pyógenes caracterizada por eritema e intenso dolor con rápida progresión asociada a shock tóxico", "fasceitis necrotizante"),
-	new Word(6, "G", "Empieza por G:", " Vector de Borrelia burgdorferi  y Erlichia canis ", "garrapata"),
-	new Word(7, "H", "Empieza por H:", " Sindrome de activación inmune caracterizado por fiebre, citopenias, esplenomegalia, hipertrigliceridemia, hipofibrinogenemia, elevación de ferritina y alta mortalidad", "hemofagocitico"),
-	new Word(8, "I", "Empieza por I:", " Antituberculoso usado tanto para tratamiento como profilaxis", "isoniazida"),
-	new Word(9, "J", "Contiene la J:", " Nombre de hongo patógeno oportunista que coloniza el aparato respiratorio, antiguamente considerado parásito", "pneumocystis jirovecii"),
-	new Word(10, "L", "Empieza por L:", " Especie de Candida resistente a anfotericina B", "lusitaniae"),
-	new Word(11, "M", "Empieza por M:", " Síndrome caracterizado por triada fiebre, faringitis y adenopatías.", "mononucleosis infecciosa"),
-	new Word(12, "N", "Empieza por N:", " Medicamento de elección para el tratamiento de enfermedad de chagas.", "nifurtimox"),
-	new Word(13, "Ñ", "Contiene la Ñ:", " Nombre de enfermedad producida por Bartonella henselae.", "arañazo de gato"),
-	new Word(14, "O", "Empieza por O:", " Subtipo de E. Coli causante de SHU.", "o157h7"),
-	new Word(15, "P", "Empieza por P:", " Agente causal de hidrops neonatal. En preescolares se caracteriza por “el signo de la cachetada”", "parvovirus b19"),
-	new Word(16, "Q", "Empieza por Q:", " Especie de Bartonella causante de endocarditis y fiebre de las trincheras", "quintana"),
-	new Word(17, "R", "Empieza por R:", " Síndrome caracterizado por la triada: hipoacusia neurosensorial, cardiopatía congénita y anomalías oculares.", "rubeola congenita"),
-	new Word(18, "S", "Empieza por S:", " Nombre de microorganismo portador del gen AMPc.", "serratia marcescens"),
-	new Word(19, "T", "Empieza por T:", " Infección que causa eosinofilia en niños y es transmitida por perros", "toxocara canis"),
-	new Word(20, "U", "Empieza por U:", " País con alto índice de SAMR en Latinoamérica.", "uruguay"),
-	new Word(21, "V", "Empieza por V:", " Medicamento utilizado para el tratamiento de citomegalovirus congénito.", "valganciclovir"),
-	new Word(22, "X", "Empieza por X:", " Hallazgo que puede aparecer en el LCR en las encefalitis herpéticas.", "xantocromia"),
-	new Word(23, "Y", "Contiene la Y:", " Agente causal de la Peste bubónica.", "yersinia pestis"),
-	new Word(24, "Z", "Contiene la Z:", " Tratamiento antiviral profiláctico utilizado para prevención de transmisión vertical en recién nacidos.", "zidovudina")
-];
+// var words = [
+// 	new Word(0, "A", "Empieza por A:", " Nombre científico de helminto cuyas larvas alcanzan el tejido pulmonar pudiendo causar tos, crisis obstructivas y hemoptisis.", "ascaris lumbricoides"),
+// 	new Word(1, "B", "Empieza por B:", " Entidad clínica  caracterizada por fiebre sin foco, sed orina no inflamatorio, GB >15000 y PCR >90", "bacteremia oculta"),
+// 	new Word(2, "C", "Empieza por C:", " Agente infeccioso cuya infección in útero puede producir sordera en el niño", "citomegalovirus"),
+// 	new Word(3, "D", "Empieza por D:", " Antimicrobiano usado en el tratamiento del acné, rosácea, infecciones por Rickettsias, brucella canis y gonorrea", "doxiciclina"),
+// 	new Word(4, "E", "Empieza por E:", " Forma de presentación de la infección neurológica por virus herpes", "encefalitis"),
+// 	new Word(5, "F", "Empieza por F:", " Infección cutánea por S. pyógenes caracterizada por eritema e intenso dolor con rápida progresión asociada a shock tóxico", "fasceitis necrotizante"),
+// 	new Word(6, "G", "Empieza por G:", " Vector de Borrelia burgdorferi  y Erlichia canis ", "garrapata"),
+// 	new Word(7, "H", "Empieza por H:", " Sindrome de activación inmune caracterizado por fiebre, citopenias, esplenomegalia, hipertrigliceridemia, hipofibrinogenemia, elevación de ferritina y alta mortalidad", "hemofagocitico"),
+// 	new Word(8, "I", "Empieza por I:", " Antituberculoso usado tanto para tratamiento como profilaxis", "isoniazida"),
+// 	new Word(9, "J", "Contiene la J:", " Nombre de hongo patógeno oportunista que coloniza el aparato respiratorio, antiguamente considerado parásito", "pneumocystis jirovecii"),
+// 	new Word(10, "L", "Empieza por L:", " Especie de Candida resistente a anfotericina B", "lusitaniae"),
+// 	new Word(11, "M", "Empieza por M:", " Síndrome caracterizado por triada fiebre, faringitis y adenopatías.", "mononucleosis infecciosa"),
+// 	new Word(12, "N", "Empieza por N:", " Medicamento de elección para el tratamiento de enfermedad de chagas.", "nifurtimox"),
+// 	new Word(13, "Ñ", "Contiene la Ñ:", " Nombre de enfermedad producida por Bartonella henselae.", "arañazo de gato"),
+// 	new Word(14, "O", "Empieza por O:", " Subtipo de E. Coli causante de SHU.", "o157h7"),
+// 	new Word(15, "P", "Empieza por P:", " Agente causal de hidrops neonatal. En preescolares se caracteriza por “el signo de la cachetada”", "parvovirus b19"),
+// 	new Word(16, "Q", "Empieza por Q:", " Especie de Bartonella causante de endocarditis y fiebre de las trincheras", "quintana"),
+// 	new Word(17, "R", "Empieza por R:", " Síndrome caracterizado por la triada: hipoacusia neurosensorial, cardiopatía congénita y anomalías oculares.", "rubeola congenita"),
+// 	new Word(18, "S", "Empieza por S:", " Nombre de microorganismo portador del gen AMPc.", "serratia marcescens"),
+// 	new Word(19, "T", "Empieza por T:", " Infección que causa eosinofilia en niños y es transmitida por perros", "toxocara canis"),
+// 	new Word(20, "U", "Empieza por U:", " País con alto índice de SAMR en Latinoamérica.", "uruguay"),
+// 	new Word(21, "V", "Empieza por V:", " Medicamento utilizado para el tratamiento de citomegalovirus congénito.", "valganciclovir"),
+// 	new Word(22, "X", "Empieza por X:", " Hallazgo que puede aparecer en el LCR en las encefalitis herpéticas.", "xantocromia"),
+// 	new Word(23, "Y", "Contiene la Y:", " Agente causal de la Peste bubónica.", "yersinia pestis"),
+// 	new Word(24, "Z", "Contiene la Z:", " Tratamiento antiviral profiláctico utilizado para prevención de transmisión vertical en recién nacidos.", "zidovudina")
+// ];
 
 // Functions
 // -----------------------------------------------------------------------------
+
+
+function createWord(num, letter, question, answer) {
+	return new Word(num, letter, `Empieza por ${letter}:`, ` ${question}`, answer)
+}
 
 function Word(idNumber, letter, hint, definition, word, correct) {
 	this.idNumber = idNumber;
@@ -46,7 +68,7 @@ function showDefinition(pos) {
 	$("#js--definition").html(words[pos].definition);
 }
 
-var remainingWords = 25;
+var remainingWords = words.length;
 
 function checkAnswer(pos) {
 	var userAnswer = $("#js--user-answer").val().toLowerCase();
@@ -71,7 +93,7 @@ function pasapalabra(pos) {
 }
 
 function continuePlaying() {
-	if (count != 25) {
+	if (count != words.length) {
 		$("#js--user-answer").val("");
 		showDefinition(count);
 	} else {
@@ -121,7 +143,7 @@ function showUserScore() {
 
 // New game
 var count = 0; // Counter for answered words
-$("#js--new-game").click(function() {
+$("#js--new-game").click(function () {
 	$("#js--ng-controls").addClass("hidden");
 	$("#js--question-controls").removeClass("hidden");
 	$("#js--close").removeClass("hidden");
@@ -130,13 +152,13 @@ $("#js--new-game").click(function() {
 });
 
 // Send the answer
-$("#js--send").click(function() {
+$("#js--send").click(function () {
 	checkAnswer(count);
 	continuePlaying();
 });
 
 // Key bindings for send the answer
-$("#js--question-controls").keypress(function(event) {
+$("#js--question-controls").keypress(function (event) {
 	var keycode = (event.keyCode ? event.keyCode : event.which);
 	if (keycode == "13") {
 		checkAnswer(count);
@@ -145,13 +167,13 @@ $("#js--question-controls").keypress(function(event) {
 });
 
 // Skip the word
-$("#js--pasapalabra").click(function() {
+$("#js--pasapalabra").click(function () {
 	pasapalabra(count);
 	continuePlaying();
 });
 
 // Key bindings for skip the word
-$("#js--question-controls").keypress(function(event) {
+$("#js--question-controls").keypress(function (event) {
 	var keycode = (event.keyCode ? event.keyCode : event.which);
 	if (keycode == "17") {
 		pasapalabra(count);
@@ -160,11 +182,11 @@ $("#js--question-controls").keypress(function(event) {
 });
 
 // Play again
-$("#js--pa").click(function() {
+$("#js--pa").click(function () {
 	location.reload()
 });
 
 // End the game
-$("#js--close").click(function() {
+$("#js--close").click(function () {
 	endGame();
 });
