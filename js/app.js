464e2943f6925a720c1724f94a42abef1033ffc7
Feipe Reyes Z <morksoldat@gmail.com>
2019-12-15 23:46:52 -0300

    Added Google Analytics script in index.html and list of words in app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,141 @@
 const questions = {
 	"A": [
 		["Nombre científico de helminto cuyas larvas alcanzan el tejido pulmonar pudiendo causar tos, crisis obstructivas y hemoptisis.", "ascaris lumbricoides"],
-		["Planta grande", "arbol"]
+		["Antiparasitario de elección para el tratamiento de la toxocariasis", "albendazol"],
+		["Aminoglicósido de uso parenteral para el tratamiento de ITU febril o intolerancia gástrica para el tratamiento vía oral", "amikacina"],
+		["Vector de la Malaria (género)", "anopheles"],
+		["Vector de zika", "aedes aegypti"]
 	],
 	"B": [
-		["barco pequeño", "bote"],
-		["pelo de la cara", "barba"]
+		["Entidad clínica  caracterizada por fiebre sin foco, sed orina no inflamatorio, GB >15000 y PCR >90", "bacteremia oculta"],
+		["Agente etiológico de la tos convulsiva", "bordetella pertussis"],
+		["Zoonosis adquirida por contacto con perros con infección del tracto genital. En el humano puede producir infección osteoarticular.", "brucella canis"],
+		["Agente etiológico de la enfermedad de arañazo de gato", "bartonella henselae"],
+		["Agente etiológico de la Fiebre de Oroya", "bartonella baciliformis"]
+	],
+	"C": [
+		["Agente infeccioso cuya infección in útero puede producir sordera en el niño.", "citomegalovirus"],
+		["Hongo que produce meningitis en pacientes infectados con VIH con inmunodepresión severa.", "crytococcus neoformans"],
+		["Antibiótico de elección para el tratamiento intravenoso ambulatorio de la bacteremia oculta.", "ceftriaxona"],
+		["Agente causal de conjuntivitis y neumonía neonatal", "chlamydia trachomatis"],
+		["Enfermedad cuyo vector es el triatoma infestans", "chagas"]
+	],
+	"D": [
+		["Enfermedad cuyo vector es el triatoma infestans.", "doxiciclina"],
+		["Antibiótico bacteriostático usado para el tratamiento de lepra y como profilaxis de infecciones por Pneumocystis jirovecii y Toxoplasma gondii", "dapsona"],
+		["Infección por arbovirus que causa fiebre, mialgias y trombocitopenia.", "dengue"],
+		["Infección por arbovirus que causa fiebre, mialgias y trombocitopenia.", "difteria"]
+	],
+	"E": [
+		["Forma de presentación de la infección neurológica por virus herpes.", "encefalitis"],
+		["Macrólido usado en el tratamiento de la infección por Bordetella pertussis.", "eritromicina"],
+		["Sindrome causado por Parvovirus B19", "eritema infeccioso"],
+		["Enfermedad para la cual se utilizan de Criterios de Duke", "endocarditis"]
+	],
+	"F": [
+		["Infección cutánea por S. pyógenes caracterizada por eritema e intenso dolor con rápida progresión asociada a shock tóxico", "fasceitis necrotizante"],
+		["Hongo filamentoso ampliamente distribuido en el suelo y plantas que puede causar infecciones localizadas como queratitis o infecciones diseminadas en pacientes neutropénicos.", "fusarium solanii"],
+		["Los criterios de Jones son utilizados para:", "fiebre reumatica"],
+		["Nombre común de la influenza en inglés", "flu"]
+	],
+	"G": [
+		["Vector de Borrelia burgdorferi  y Erlichia canis.", "garrapatas"],
+		["Animal que transmite la bacteria Bartonella henselae a través de mordedura o lamido", "gato"],
+		["Biomarcador utilizado para el diagnóstico de Aspergilosis.", "galactomanano"],
+		["Especia de Candida", "glabrata"]
+	],
+	"H": [
+		["Sindrome de activación inmune caracterizado por fiebre, citopenias, esplenomegalia, hipertrigliceridemia, hipofibrinogenemia, elevación de ferritina y alta mortalidad.", "hemofagocitico"],
+		["Bacilo gram negativo asociado a úlcera péptica y a cáncer gástrico.", "helicobacter pylori"],
+		["Agente causal de enfermedad inmunoprovenible que puede producir neumonía, meningitis o celulitis facial.", "haemophilus influenzae"],
+		["Virus causante de Sindrome Cardiopulmonar o Sindrome Renal", "hanta"]
+	],
+	"I": [
+		["Antituberculoso usado tanto para tratamiento como profilaxis.", "isoniazida"],
+		["Parásito intestinal cuya infección en pacientes infectados con VIH traducida en diarrea de más de 1 mes de evolución se considera una Enfermedad Indicadora de SIDA.", "isospora belli"],
+		["Antibiótico de la familia de los carbapenémico.", "imipenem"],
+		["Tratamiento de primera línea para enfermedad de Kawasaki.", "inmunoglobulina"]
+	],
+	"J": [
+		["Nombre de hongo patógeno oportunista que coloniza el aparato respiratorio, antiguamente considerado parásito.", "pneumocystis jirovecii"],
+		["RAM severa que compromete piel, mucosas, fanéreos, intestino, pulmón de evolución grave e incluso fatal.", "Stevens Johnson"]
+	],
+	"K": [
+		["Carbapenemasa plasmidial de rápida diseminación (siglas).", "kpc"],
+		["Aagente infeccioso causante de infecciones osteoarticulares en niños menores de 5 años.", "kingella kingae"],
+		["Antibiótico utilizado para microorganismos BLEE.", "amikacina"],
+		["Enfermedad que puede causar hidrops vesicular.", "kawasaki"]
+	],
+	"L": [
+		["Especie de Candida resistente a anfotericina B", "lusitaniae"],
+		["Estado característico de los virus herpes, en la cual el genoma viral permanece dentro de las células del huésped durante toda la vida.", "latencia"],
+		["Especia bacteriana que causa sepsis neonatal.", "listeria monocytogenes"],
+		["Nombre sindromático de toxocara", "larva migrans visceral"]
+	],
+	"M": [
+		["Síndrome caracterizado por triada fiebre, faringitis y adenopatías.", "mononucleosis infecciosa"],
+		["Síndrome caracterizado por inflamación del músculo cardiaco. Dentro de sus causas se encuentran, enterovirus, adenovirus y parvovirus B 19.", "miocarditis"],
+		["Agente etiológico de otitis media aguda", "moraxella catarrhalis"],
+		["Complicación de la influenza", "miositis"]
+	],
+	"N": [
+		["Medicamento de elección para el tratamiento de enfermedad de chagas.", "nifurtimox"],
+		["Antibiótico utilizado para la ITU baja.", "nitrofurantoina"],
+		["Tratamiento de Crypstosporidium", "nitazoxanida"],
+		["Antifúngico útil en candidiasis del pañal", "nistatina"]
+	],
+	"O": [
+		["Subtipo de E. Coli causante de SHU","o157h7"],
+		["Infección fúngica de la uña", "onicomicosis"]
+	],
+	"P": [
+		["Nombre de agente causal de hidrops neonatal. En preescolares se caracteriza por el signo de la cachetada.", "parvovirus b19"],
+		["Bacilo gram negativo no fermentador", "pseudomonas aeruginosa"],
+		["Ser vivo que vive y se nutre de otro sin aportarle ningún tipo de beneficio a este último.", "parasito"],
+		["Enfermedad inmunoprevenible que causa aumento de volumen parotideo", "parotiditis"]
+	],
+	"Q": [
+		["Especie de Bartonella causante de endocarditis y fiebre de las trincheras", "quintana"],
+		["Término que se refiere a la presencia de células linfohematopoyéticas no propias del receptor que aparecen como resultado de un trasplante alogénico", "quimerismo"]
+	],
+	"R": [
+		["Síndrome caracterizado por la triada: hipoacusia neurosensorial, cardiopatía congénita y anomalías oculares.", "rubeola congenita"],
+		["Zoonosis transmitida por murciélagos.","rabia"],
+		["Organelo celular sitio de acción de la clindamicina", "ribosoma"],
+		["Bacteria causante de Enfermedad de Lyme.", "rickettsia rickettsii"]
+	],
+	"S": [
+		["Especie bacteriana portadora del gen AMPc.", "serratia marcescens"],
+		["Género bacteriano bacilo gram negativo que causa infección posterior a ingerir huevos mal cocidos.", "salmonella"],
+		["Enfermedad causada por parásito Sarcopteis scabei", "sarna"],
+		["Enfermedad inmunoprevenible de notificación inmediata.", "sarampion"]
+	],
+	"T": [
+		["Infección que causa eosinofilia en niños y es transmitida por perros.", "toxocara canis"],
+		["Parásito intracelular que causa enfermedad de Chagas.", "trypanosoma cruzi"],
+		["Vector de Enfermedad de Chagas en Chile.", "triatoma infestans"],
+		["Medicamento inmunosupresor que aumenta el riesgo de reactivación de infecciones virales en trasplantados.", "timoglobulina"]
+	],
+	"U": [
+		["País con alto índice de SAMR en Latinoamérica.", "uruguay"],
+		["Infección de lámina intermedia del ojo, situada entre la esclerótica y la retina", "uveitis"]
+	],
+	"V": [
+		["Medicamento utilizado para el tratamiento de citomegalovirus congénito.", "valganciclovir"],
+		["Principal causa de meningitis viral.", "enterovirus"],
+		["Bacilo gram negativo que causa infección intestinal aguda por la ingestión de alimentos o agua contaminados.", "vbrio cholerae"],
+		["Retrovirus cuyo principal receptor molecular se encuentra en los linfocitos T CD4 (sigla).", "vih"]
+	],
+	"X": [
+		["Nombre comercial de método diagnóstico molecular para diagnóstico de Mycobacterium tuberculosis, que además identifica resistencia a rifampicina.", "genexpert"]
+	],
+	"Y": [
+		["Agente causal de la Peste bubónica.", "yersinia pestis"],
+		["Levadura en inglés", "yeast"]
+	],
+	"Z": [
+		["Tratamiento antiviral profiláctico utilizado para prevención de transmisión vertical en recién nacidos.", "zidovudina"],
+		["Arbovirus que puede producir microcefália congénita", "zika"]
 	]
 }
 const questions_array = Object.entries(questions)
